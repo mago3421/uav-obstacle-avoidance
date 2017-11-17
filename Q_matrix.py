c0fe1544f4bddbb058f06091b36d0bb516d8c3d7
Ramya <raka1840@colorado.edu>
2017-11-17 11:20:31 -0700

    changes made

--- Q_matrix.py
+++ Q_matrix.py
@@ -24,19 +24,23 @@ class Q_matrix():
     location = self.entities["agent"],
     rewards = self.rewards or self.los # in uav
     
-    def update(self,location,rewards):
+    def update(self,location,los):
         st = location[0]*self.grid_sz + location[1]
         for i in range(self.num_actions): #number of actions
             if i == 0:   #action is up
                 st_new = location[0]*self.grid_sz + location[1]+1
+                act = "up"
             elif i == 1: #action is down
                 st_new = location[0]*self.grid_sz + location[1]-1
+                act = "down"
             elif i == 2:   #action is left
                 st_new = (location[0]-1)*self.grid_sz + location[1]
+                act = "left"
             elif i == 3: #action is right
                 st_new = (location[0]+1)*self.grid_sz + location[1]
+                act = "right"
                     
-            self.Q[st,i] = (1-self.alpha)*self.Q[st,i] + self.alpha*(rewards(st,i) + self.gamma*max(Q[st_new,:]))
+            self.Q[st,i] = (1-self.alpha)*self.Q[st,i] + self.alpha*(los[act] + self.gamma*max(Q[st_new,:]))
         
         a = self.Q.index(max(self.Q[st,:])) 
 		
